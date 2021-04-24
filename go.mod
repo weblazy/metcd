6d14e8791569953ddabed60f3acdd072711e0a0c
liuguoqiang <liuguoqiang@xiaoheiban.cn>
2021-04-24 16:00:53 +0800

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/weblazy/metcd
+
+go 1.14
+
+require (
+	github.com/coreos/etcd v3.3.25+incompatible // indirect
+	github.com/etcd-io/etcd v3.2.32+incompatible
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/prometheus/client_golang v1.10.0 // indirect
+	go.etcd.io/etcd/server/v3 v3.5.0-alpha.0
+)
---
+++ kvstore.go
@@ -0,0 +1,16 @@
+package metcd
+
+type KVStore interface {
+	// Lookup get key value
+	Lookup(key string) (string bool)
+	// Propose kv request into raft state machine
+	Propose(k, v string)
+	// ReadCommits consume entry from raft state machine into KvStore map until error
+	ReadCommits(commitC <-chan *string, errorC <-chan error)
+	// SnapShot return KvStore snapshot
+	SnapShot() ([]byte, error)
+	// RecoverFromSnapshot recover from snapshot
+	RecoverFromSnapshot(snapshot []byte) error
+	// Close close backend databases
+	Close() error
+}
