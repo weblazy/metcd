008a2a84481a1bdce78baf3de362837e9ee171f0
lazy <2276282419@qq.com>
2021-10-27 17:07:06 +0800

    feat: log

--- main/main.go
+++ main/main.go
@@ -22,7 +22,7 @@ import (
 	"go.etcd.io/etcd/raft/v3/raftpb"
 )
 
-// 保护测试1
+// 保护测试2
 func main() {
 	cluster := flag.String("cluster", "http://127.0.0.1:9021", "comma separated cluster peers")
 	id := flag.Int("id", 1, "node ID")
